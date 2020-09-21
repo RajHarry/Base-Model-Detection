3d53c8909bd4f5e62b7f33a1528969dbc561d1b3
RajHarry <rajharry418@gmail.com>
2020-09-22 00:36:26 +0530

    working model

--- App/models.py
+++ App/models.py
@@ -12,5 +12,5 @@ class DynamicUpload(models.Model):
     image = models.ImageField(upload_to="dynamic/", null=True, blank=True)
     name = models.CharField(max_length=32, blank=True)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
--- App/views.py
+++ App/views.py
@@ -10,11 +10,16 @@ from django.shortcuts import render
 import time
 import base64
 import numpy as np
-import os,io
+import os,io,re
 from FaceMaskDetection.ssd_opencv import inference
 import requests
 from PIL import Image
 import json
+import datetime
+
+def get_current_time():
+    e = datetime.datetime.now()
+    return str(e.day)+"_"+str(e.month)+"_"+str(e.year)+"_"+str(e.hour)+"_"+str(e.minute)+"_"+str(e.second)
 
 def index(request):
     static_images = glob.glob("media/static-img/*")
@@ -38,24 +43,8 @@ def image_details_bk(request):
     response_data['image_url'] = "media/dynamic/"+file_name
     return JsonResponse(response_data)
 
-def image_details(request):
-    response_data = {}
-    #print(request.FILES)
-    browse_image = request.FILES['browse_image']
-    #print(browse_image)
-    file_name = str(browse_image).replace(" ","_").replace("&","")
-    model = DynamicUpload(image=browse_image, name= file_name)
-    model.save()
-
-    url = "http://127.0.0.1:8000/model_process_image"
-    img_path = "media/dynamic/"+file_name
-    payload = {"image": open(img_path, "rb")}
-    r = requests.post(url, files=payload,data={"model":"SSD"},verify=False).json()
-    #print("r",r)
-    response_data["encoded_image"] = r[0]['encoded_image']
-    response_data['file_name'] = file_name
-    response_data['image_url'] = "media/dynamic/"+file_name
-    return JsonResponse(response_data)
+def remove_symbols(file_name):
+    return str(re.sub(r'[^\w]', '', file_name))
 
 def _grab_image(path=None, stream=None, url=None):
         # if the path is not None, then load the image from disk
@@ -104,43 +93,80 @@ def model_process(request):
         return JsonResponse(data,safe=False)
 
 @csrf_exempt
-def model_process_image(request):
-        print("Model Used: ",request.POST.get("model"))
-        data = [{"success": False}]
-        # check to see if this is a post request
-        if request.method == "POST":
-                print("in")
-                # check to see if an image was uploaded
-                if request.FILES.get("image", None) is not None:
-                        # grab the uploaded image
-                        image = _grab_image(stream=request.FILES["image"])
-                # otherwise, assume that a URL was passed in
-                else:
-                        # grab the URL from the request
-                        url = request.POST.get("url", None)
-                        # if the URL is None, then return an error
-                        if url is None:
-                                data["error"] = "No URL provided."
-                                return JsonResponse(data)
-                        # load the image and convert
-                        image = _grab_image(url=url)
-                if(request.POST.get('model') == "SSD"):
-                        print(">>>")
-                        output_info, img_output,out_image = inference(image, draw_result=True, target_shape=(260, 260))
-                elif(request.POST.get('model') == "YOLO"):
-                        out_image = yolo_image_process(image)
-                else:
-                        print("Something is Wring>>")
-                # base64_encoded = base64.b64encode(image)
-                cv2.imwrite("cv2_image_recorded.jpg",out_image)
-                with open('cv2_image_recorded.jpg', 'rb') as imageFile:
-                    str_en = "data:image/jpeg;base64,"+str(base64.b64encode(imageFile.read())).replace("b'","")[:-1]
-                #print(str_en)
-                #print(base64_encoded)
-                data = [{'success':True,'encoded_image':str(str_en)}]
-                # else:
-                #       print("else block")
-        return JsonResponse(data,safe=False)
+def model_process_image_bk(request):
+    print("Model Used: ",request.POST.get("model"))
+    data = [{"success": False}]
+    # check to see if this is a post request
+    if request.method == "POST":
+        print("in")
+        # check to see if an image was uploaded
+        if request.FILES.get("image", None) is not None:
+            # grab the uploaded image
+            image = _grab_image(stream=request.FILES["image"])
+        # otherwise, assume that a URL was passed in
+        else:
+            # grab the URL from the request
+            url = request.POST.get("url", None)
+            # if the URL is None, then return an error
+            if url is None:
+                    data["error"] = "No URL provided."
+                    return JsonResponse(data)
+            # load the image and convert
+            image = _grab_image(url=url)
+        if(request.POST.get('model') == "SSD"):
+                print(">>>")
+                output_info, img_output,out_image = inference(image, draw_result=True, target_shape=(260, 260))
+        elif(request.POST.get('model') == "YOLO"):
+                out_image = yolo_image_process(image)
+        else:
+                print("Something is Wring>>")
+        # base64_encoded = base64.b64encode(image)
+        cv2.imwrite("cv2_image_recorded.jpg",out_image)
+        with open('cv2_image_recorded.jpg', 'rb') as imageFile:
+            str_en = "data:image/jpeg;base64,"+str(base64.b64encode(imageFile.read())).replace("b'","")[:-1]
+        #print(str_en)
+        #print(base64_encoded)
+        data = [{'success':True,'encoded_image':str(str_en)}]
+        # else:
+        #       print("else block")
+    return JsonResponse(data,safe=False)
 
-        # def index(request):
-        #     return render(request,'index.html')
+
+
+def model_process_image(path,model):
+    image = cv2.imread(path)
+    if(model == "SSD"):
+        output_info, img_output,out_image = inference(image, draw_result=True, target_shape=(260, 260))
+    elif(model == "YOLO"):
+        out_image = yolo_image_process(image)
+    else:
+        print("Something is Wring>>")
+    # base64_encoded = base64.b64encode(image)
+    image_path = "media/results/"+path.split("/")[-1]
+    cv2.imwrite(image_path,out_image)
+    
+    return image_path
+@csrf_exempt
+def image_details(request):
+    response_data = {}
+    print("request.post: ",request.POST['model'])
+    if(request.POST.get('static') == "yes"):
+        image_path = "media/static-img/Image"+request.POST['number']+".jpeg"
+    else: 
+        browse_image = request.FILES['browse_image']
+        name,ext = str(browse_image).split(".")
+        file_name = get_current_time()+"."+ext
+
+        model = DynamicUpload(image=browse_image, name= str(file_name))
+        model.save()
+
+        obj = DynamicUpload.objects.get(name=file_name)
+        image_path = "media/"+obj.image.name
+    print("Image Path: ",image_path)
+    # url = "model_process_image"
+    # payload = {"image": open(dynamic_image_path, "rb")}
+    # r = requests.post(url, files=payload,data={"model":"SSD"},verify=False).json()
+    result_image_stored_path = model_process_image(image_path,"SSD")
+
+    response_data['image_url'] = result_image_stored_path
+    return JsonResponse(response_data)
